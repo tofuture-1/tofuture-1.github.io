265aff6cee17f330c05cf84f816169dc107ac961
tofuture-1 <2415803689@qq.com>
2025-03-02 23:25:01 +0800

    Site updated: 2025-03-02 23:25:01

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/24/index/","2025/02/24/test1/"];function toRandomPost(){
+var posts=["2025/02/24/test1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
