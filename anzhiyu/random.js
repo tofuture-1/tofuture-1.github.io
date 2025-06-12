6c6d88d09f192fd5777befc912aec75ef9c00e26
tofuture-1 <2415803689@qq.com>
2025-06-12 16:24:25 +0800

    Site updated: 2025-06-12 16:24:24

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/16/综合应用实例/","2025/02/24/相关信源网站汇总/","2025/03/03/网站简单使用指南/"];function toRandomPost(){
+var posts=["2025/05/15/媒介素养/","2025/05/01/杂记/","2025/02/24/相关信源网站汇总/","2025/03/03/网站简单使用指南/","2025/03/16/综合应用实例/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
